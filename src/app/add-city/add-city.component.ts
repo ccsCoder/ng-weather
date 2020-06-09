f89113b50720500566ff8f86be033afdd6772192
Nishant Gupta <nishantgupta@neo-mac-pro.local>
2020-06-09 17:35:33 +0530

    API working.

--- src/app/add-city/add-city.component.ts
+++ src/app/add-city/add-city.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
 import { Subject } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-city',
@@ -11,26 +11,51 @@ import { debounceTime, map } from 'rxjs/operators';
 export class AddCityComponent implements OnInit {
 
   cities: string[];
+  searchTextChanged = new Subject<string>();
+  suggestionsList: Element;
+  message = '';
+  isError = false;
+  selectedCity = null;
 
   constructor(private weatherService: WeatherService) {
   }
 
-  setCities(cityData): void {
-    this.cities = cityData.map(datum => `${datum.name}, ${datum.country.id}`);
-    console.log(this.cities);
-  }
-
-  onCityInput(cityName): void {
-    if (cityName.length < 3) {
+  fetchCities(cityPrefix: string): void {
+    // dont make any server call for < 3 characters
+    if (cityPrefix.trim().length < 3) {
+      this.isError = true;
+      this.message = 'Please type 3 or more characters to search';
       return;
     }
-    this.weatherService.getCityNames(cityName).pipe(debounceTime(1000)).subscribe(cityData => {
+    this.weatherService.getCityNames(cityPrefix).subscribe(cityData => {
       this.setCities(cityData);
     });
-
   }
 
   ngOnInit(): void {
+    this.suggestionsList = document.querySelector('#city-list');
+    // test
+    console.log(this.suggestionsList);
+
+    this.searchTextChanged.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+    ).subscribe(searchText => {
+      console.log(`SearchText: ${searchText}`);
+      // make the API call now
+      this.fetchCities(searchText);
+    });
   }
 
+  onKeyUp(cityName: string): void {
+    this.selectedCity = cityName.replace(/,[^,]*$/, '');
+  }
+
+  setCities(cityData): void {
+    this.cities = cityData.map(datum => `${datum.name}, ${datum.country.id}`);
+  }
+
+  onCityInput(cityName: string): void {
+    this.searchTextChanged.next(cityName.replace(/,[^,]*$/, ''));
+  }
 }
